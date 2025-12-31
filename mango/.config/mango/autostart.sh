2afb2818461624ad9ec0c66bc9efbd06cd63675f
Brian <Brliu385@gmail.com>
2025-12-31 17:07:18 -0500

    fixed issue with autostart.sh not starting with wm

--- mango/.config/mango/autostart.sh
+++ mango/.config/mango/autostart.sh
@@ -1,4 +1,3 @@
-#! /bin/zsh
 waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
 swaybg -i /extra/Github/Wallpapers/Studio-Ghibli-HD-Wallpaper-Free-download.jpg >/dev/null 2>&1 &
 
