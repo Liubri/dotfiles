0b2fc692c44f309cfa5c0f266731ef4efd301b4b
Brian <Brliu385@gmail.com>
2026-01-01 18:30:41 -0500

    clipboard persist for wayland

--- mango/.config/mango/autostart.sh
+++ mango/.config/mango/autostart.sh
@@ -1,3 +1,5 @@
 waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
 swaybg -i /extra/Github/Wallpapers/Studio-Ghibli-HD-Wallpaper-Free-download.jpg >/dev/null 2>&1 &
+wl-clip-persist --clipboard regular --reconnect-tries 0 &
+wl-paste --type text --watch cliphist store &
 
